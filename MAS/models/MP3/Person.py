e14a70d2647f380204ca6170524e803d141cfb60
wjureczka <wjureczka@gmail.com>
2020-06-15 18:52:31 +0200

    Ready to MP4 take off

--- MAS/models/MP3/Person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    type = "Person"
-
-    def __init__(self, name, sex="other"):
-        self.sex = sex
-        self.name = name
-
-    def getName(self):
-        return self.name
