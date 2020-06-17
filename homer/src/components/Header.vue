5faff71b47dcaafcee149f996292fa154d9a7891
wjureczka <wjureczka@gmail.com>
2020-06-18 00:00:39 +0200

    Add frontend app

---
+++ homer/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <ul class="nav__links">
+        <li>
+          A
+        </li>
+
+        <li>
+          B
+        </li>
+
+        <li>
+          C
+        </li>
+
+        <li>
+          D
+        </li>
+
+        <li>
+          E
+        </li>
+      </ul>
+
+      <div class="nav__user">
+
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+  .header {
+    width: 100%;
+    display: block;
+    background: green;
+  }
+
+  .nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+
+    &__links {
+    justify-content: center;
+    align-items: center;
+    }
+
+    &__user {
+    justify-content: center;
+    align-items: center;
+    }
+  }
+</style>
---
+++ homer/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div>
+    <h1>
+      Login into your account
+    </h1>
+
+    <form>
+
+      <label for="email">Email</label>
+      <input id="email" type="email">
+
+      <label for="password">Password</label>
+      <input id="password" type="password">
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
